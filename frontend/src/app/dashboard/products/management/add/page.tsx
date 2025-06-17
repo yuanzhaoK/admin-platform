1c19191256eb4621fc2b3fc4212f91ed61a10fd6
KunpengYu <yukunpeng@tanka.ai>
2025-06-17 10:12:20 +0800

    fix code error

--- frontend/src/app/dashboard/products/management/add/page.tsx
+++ frontend/src/app/dashboard/products/management/add/page.tsx
@@ -10,7 +10,7 @@ import { Textarea } from '@/components/ui/textarea'
 import { 
   Select,
   SelectContent,
-  SelectItem,·  
+  SelectItem,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
